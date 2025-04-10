0e7151c439d34a0d4c0bb245b460f0ff458ed138
hnlgrossman <gha.github@gmail.com>
2025-04-10 14:32:12 +0300

    add pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,32 @@ export default function RootLayout({
         <WhatsAppButton />
         <UserTracker />
         <Script src="https://cdn.enable.co.il/licenses/enable-L2556825acdsy3oi-0424-69920/init.js" />
+        
+        {/* Meta Pixel Code */}
+        <Script id="facebook-pixel">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1380261343172555');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1380261343172555&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </body> 
     </html>
   );
